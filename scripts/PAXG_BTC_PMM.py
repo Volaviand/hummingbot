cf85100200f79b8ce437f8704c568f3b069850d1
Volaviand <34086127+Volaviand@users.noreply.github.com>
2024-04-08 10:35:18 -0600

    fill tracker error. Resetting so i can not have it waste space, but may have an issue with tracking

--- scripts/PAXG_BTC_PMM.py
+++ scripts/PAXG_BTC_PMM.py
@@ -125,7 +125,7 @@ class SimplePMM(ScriptStrategyBase):
         self.entry_percents = self.geometric_entry_levels()
 
 
-        self.buy_counter = 2
+        self.buy_counter = 1
         self.sell_counter = 1
     def on_tick(self):
         if self.create_timestamp <= self.current_timestamp:
@@ -495,7 +495,7 @@ class SimplePMM(ScriptStrategyBase):
             if self.initialize_flag == True:
                 # Fetch midprice only during initialization
                 if self._last_trade_price is None:
-                    midprice = 0.032988 #self.connectors[self.exchange].get_price_by_type(self.trading_pair, PriceType.MidPrice)
+                    midprice = 0.0324 #self.connectors[self.exchange].get_price_by_type(self.trading_pair, PriceType.MidPrice)
                     # Ensure midprice is not None before converting and assigning
                     if midprice is not None:
                         self._last_trade_price = Decimal(midprice)
