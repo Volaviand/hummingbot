4e9dcb2ec0030ac1a0fcb6433a6441a51f34a205
Volaviand <34086127+Volaviand@users.noreply.github.com>
2024-04-13 14:33:52 -0600

    update for restart

--- scripts/PAXG_BTC_PMM.py
+++ scripts/PAXG_BTC_PMM.py
@@ -126,7 +126,7 @@ class SimplePMM(ScriptStrategyBase):
 
 
         self.buy_counter = 1
-        self.sell_counter = 1
+        self.sell_counter = 4
     def on_tick(self):
         if self.create_timestamp <= self.current_timestamp:
             self.cancel_all_orders()
@@ -495,7 +495,7 @@ class SimplePMM(ScriptStrategyBase):
             if self.initialize_flag == True:
                 # Fetch midprice only during initialization
                 if self._last_trade_price is None:
-                    midprice = 0.03240 #self.connectors[self.exchange].get_price_by_type(self.trading_pair, PriceType.MidPrice)
+                    midprice = 0.0369860 #self.connectors[self.exchange].get_price_by_type(self.trading_pair, PriceType.MidPrice)
                     # Ensure midprice is not None before converting and assigning
                     if midprice is not None:
                         self._last_trade_price = Decimal(midprice)
