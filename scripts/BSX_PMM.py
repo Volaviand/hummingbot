fe7c36129f370984fd69609fe6f0a8da8cfbf77a
Volaviand <34086127+Volaviand@users.noreply.github.com>
2024-04-13 14:59:29 -0600

    test

--- scripts/BSX_PMM.py
+++ scripts/BSX_PMM.py
@@ -494,7 +494,7 @@ class SimplePMM(ScriptStrategyBase):
             if self.initialize_flag == True:
                 # Fetch midprice only during initialization
                 if self._last_trade_price is None:
-                    midprice = 0.0001168 #self.connectors[self.exchange].get_price_by_type(self.trading_pair, PriceType.MidPrice)
+                    midprice = 0.00011510 #self.connectors[self.exchange].get_price_by_type(self.trading_pair, PriceType.MidPrice)
                     # Ensure midprice is not None before converting and assigning
                     if midprice is not None:
                         self._last_trade_price = Decimal(midprice)
