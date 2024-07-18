1bc08e6e8eb397e960efb907c4cd5eecb9fcbcd2
Volaviand <34086127+Volaviand@users.noreply.github.com>
2024-07-18 16:22:48 -0600

    put in logic for minimum profit %

--- scripts/BSX_PMM.py
+++ scripts/BSX_PMM.py
@@ -299,12 +299,12 @@ class SimplePMM(ScriptStrategyBase):
         bd = 0.01
         ## Percent multiplier, <1 = buy(goes down), >1 = sell(goes up) 
         #p = (1 - 0.05)
-        bp = bd**(1/n)
+        bp = min( 1 - self.min_profitability, bd**(1/n) )
 
         ## Sells
         ## 3 distance move,(distance starts at 1 or 100%) 200% above 100 %
         sd = 3
-        sp = (sd**(1/n)) 
+        sp = max(1 + self.min_profitability, (sd**(1/n)) )
 
         return bp, sp
 
