defc5d8e8cbc7e002b1832c544fee4f4fef596ca
Volaviand <34086127+Volaviand@users.noreply.github.com>
2024-07-18 18:27:01 -0600

    too many decimal issues. look into later.

--- scripts/BSX_PMM.py
+++ scripts/BSX_PMM.py
@@ -300,17 +300,11 @@ class SimplePMM(ScriptStrategyBase):
         ## Percent multiplier, <1 = buy(goes down), >1 = sell(goes up) 
         #p = (1 - 0.05)
         bp = min( 1 - self.min_profitability, bd**(1/n) )
-        bp = Decimal(bp)
-        bp = (Decimal(1) - bp) * order_shape_factor
-        bp = Decimal(1) - bp
 
         ## Sells
         ## 3 distance move,(distance starts at 1 or 100%) 200% above 100 %
         sd = 3
         sp = max(1 + self.min_profitability, (sd**(1/n)) )
-        sp = Decimal(sp)
-        sp = (sp - Decimal(1) ) * order_shape_factor
-        sp = sp + Decimal(1)
 
         msg = (f"sp :: {sp:.8f} , bp :: {bp:.8f}")
         self.log_with_clock(logging.INFO, msg)
