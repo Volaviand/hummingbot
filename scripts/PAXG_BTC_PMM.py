074818f79213973aa2400309795e399b6306ed96
Volaviand <34086127+Volaviand@users.noreply.github.com>
2024-07-18 23:57:47 -0600

    fix formula.

--- scripts/PAXG_BTC_PMM.py
+++ scripts/PAXG_BTC_PMM.py
@@ -292,6 +292,7 @@ class SimplePMM(ScriptStrategyBase):
     def determine_log_multipliers(self):
         """Determine the best placement of percentages based on the percentage/log values 
         (log(d)) / (log(p)) = n, breakding this down with a fixed n to solve for p value turns into  p = d**(1/n).  Or closer p = e^(ln(d) / n)"""
+
         n = math.floor(self.maximum_orders/2)
         ## Buys
         #Minimum Distance in percent. 0.01 = a drop of 99% from original value
@@ -299,7 +300,7 @@ class SimplePMM(ScriptStrategyBase):
         ## Percent multiplier, <1 = buy(goes down), >1 = sell(goes up) 
         #p = (1 - 0.05)
         #bp = min( 1 - self.min_profitability, bd**(1/n) )
-        bp = math.exp(math.log(bd/n))
+        bp = math.exp(math.log(bd)/n)
         ## Sells
         ## 3 distance move,(distance starts at 1 or 100%) 200% above 100 %
         sd = 4
