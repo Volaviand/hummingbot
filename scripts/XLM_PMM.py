b3dcb54ea3a0ad082f88047c05f01cf61a00a042
Volaviand <34086127+Volaviand@users.noreply.github.com>
2024-08-26 19:37:37 -0600

    update distances, makes buying harder since drops are wider etc. Easier to sell to balance out/ safer.

--- scripts/XLM_PMM.py
+++ scripts/XLM_PMM.py
@@ -298,14 +298,14 @@ class SimplePMM(ScriptStrategyBase):
         n = math.floor(self.maximum_orders/2)
         ## Buys
         #Minimum Distance in percent. 0.01 = a drop of 99% from original value
-        bd = 0.4
+        bd = 0.25
         ## Percent multiplier, <1 = buy(goes down), >1 = sell(goes up) 
         #p = (1 - 0.05)
         #bp = min( 1 - self.min_profitability, bd**(1/n) )
         bp = math.exp(math.log(bd)/n)
         ## Sells
         ## 3 distance move,(distance starts at 1 or 100%) 200% above 100 %
-        sd = 3.75
+        sd = 3
         #sp = max(1 + self.min_profitability, (sd**(1/n)) )
         sp = math.exp(math.log(sd)/n)
 
