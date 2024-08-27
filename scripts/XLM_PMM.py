1d5f70187904f9f8ff1f0708d84aec664d5b2eb4
Volaviand <34086127+Volaviand@users.noreply.github.com>
2024-08-26 18:22:43 -0600

    fix price

--- scripts/XLM_PMM.py
+++ scripts/XLM_PMM.py
@@ -594,7 +594,7 @@ class SimplePMM(ScriptStrategyBase):
             if self.initialize_flag == True:
                 # Fetch midprice only during initialization
                 if self._last_trade_price is None:
-                    midprice = 0.00003950 #self.connectors[self.exchange].get_price_by_type(self.trading_pair, PriceType.MidPrice)
+                    midprice = 0.08698 #self.connectors[self.exchange].get_price_by_type(self.trading_pair, PriceType.MidPrice)
                     # Ensure midprice is not None before converting and assigning
                     if midprice is not None:
                         self._last_trade_price = Decimal(midprice)
