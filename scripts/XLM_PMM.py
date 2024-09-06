6392e84e3a2465a01003f2783da847efe4a9cd86
Volaviand <34086127+Volaviand@users.noreply.github.com>
2024-09-06 16:06:19 -0600

    d

--- scripts/XLM_PMM.py
+++ scripts/XLM_PMM.py
@@ -669,10 +669,6 @@ class SimplePMM(ScriptStrategyBase):
         self.log_with_clock(logging.INFO, (f"Close {self.close_history[-1]}, last close {self.close_history[-2]}"))
         self.log_with_clock(logging.INFO, (self.log_returns[-1]))
 
-        msg_1 = (f"GARCH Volatility {garch_volatility:.8f}")
-        self.log_with_clock(logging.INFO, msg_1)
-        msg_2 = (f"GARCH Volatility {garch_volatility:.8f}")
-        self.log_with_clock(logging.INFO, msg_1)
         # Fit GARCH model to log returns
         model = arch_model(log_returns, vol='GARCH', p=3, q=3, power=2.0)
         model_fit = model.fit(disp="off")  # Fit the model without display
