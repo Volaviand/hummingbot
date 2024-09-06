b03dafa27e982bc6c5aa5e71a336b5062f8f6d38
Volaviand <34086127+Volaviand@users.noreply.github.com>
2024-09-06 14:24:42 -0600

    test

--- scripts/XLM_PMM.py
+++ scripts/XLM_PMM.py
@@ -658,15 +658,15 @@ class SimplePMM(ScriptStrategyBase):
         df = volatility_metrics_df
         close = df["close"].astype(float)
 
-        returns = np.array([] , dtype=float64)
+        returns = []
         for i in range(1, len(df)):
-            current_price = df.iloc[i]["close"]
-            previous_price = df.iloc[i-1]["close"]
-            log_return = np.log(current_price / previous_price)
+            current_price = df["close"].iloc[i]
+            previous_price = df["close"].iloc[i-1]
+            log_return = math.log(current_price / previous_price)
             returns.append(log_return)
 
         # Convert returns to a DataFrame
-        returns = returns
+        returns = pd.Series(returns, dtype=np.float64)
 
         # Drop NaN and Inf values
         returns = pd.to_numeric(returns, errors='coerce').dropna()
