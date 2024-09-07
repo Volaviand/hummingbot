d5acfa41d817c0b3a73bf33c0676fd8d8b16798b
Volaviand <34086127+Volaviand@users.noreply.github.com>
2024-09-06 18:34:50 -0600

    m

--- scripts/XLM_PMM.py
+++ scripts/XLM_PMM.py
@@ -88,8 +88,8 @@ class SimplePMM(ScriptStrategyBase):
     max_records = 720
 
     volatility_interval = 480
-    columns_to_show = ["trading_pair", "interval", "mean"]
-    sort_values_by = ["interval", "mean"]
+    columns_to_show = ["trading_pair", "interval"]
+    sort_values_by = ["interval"]
     top_n = 20
     report_interval = 60 * 60 * 6  # 6 hours
 
