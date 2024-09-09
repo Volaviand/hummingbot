8ab16994e89a57a836aae598053d905b9fbd4bb4
Volaviand <34086127+Volaviand@users.noreply.github.com>
2024-09-09 15:04:13 -0600

    test a total imbalance method, It'll balance everythong on a profitable trade now.  If i am going to go back to a non BE strategy, then perhaps i'd want to consider the other version.

--- scripts/PAXG_BTC_PMM.py
+++ scripts/PAXG_BTC_PMM.py
@@ -611,13 +611,13 @@ class SimplePMM(ScriptStrategyBase):
         #to market overcorrection
         if q > 0 :
             base_balancing_volume =  abs(minimum_size) *  Decimal.exp(-self.order_shape_factor * q)
-            quote_balancing_volume = abs(minimum_size) * ( 1 + ( 1 - Decimal.exp(-self.order_shape_factor * q))) 
+            quote_balancing_volume = total_imbalance ##abs(minimum_size) * ( 1 + ( 1 - Decimal.exp(-self.order_shape_factor * q))) 
             # Ensure base balancing volume does not exceed the amount needed to balance
             if quote_balancing_volume > total_imbalance:
                 quote_balancing_volume = total_imbalance
 
         elif q < 0 :
-            base_balancing_volume = abs(minimum_size) *  ( 1 + ( 1 - Decimal.exp(self.order_shape_factor * q)))
+            base_balancing_volume =  total_imbalance ##abs(minimum_size) *  ( 1 + ( 1 - Decimal.exp(self.order_shape_factor * q)))
             quote_balancing_volume = abs(minimum_size) * Decimal.exp(self.order_shape_factor * q) 
                
             # Ensure base balancing volume does not exceed the amount needed to balance
@@ -641,8 +641,8 @@ class SimplePMM(ScriptStrategyBase):
 
         minimum_size = self.connectors[self.exchange].quantize_order_amount(self.trading_pair, self.order_amount)
 
-        order_size_bid = base_balancing_volume #max(minimum_size, quote_balancing_volume)
-        order_size_ask = quote_balancing_volume  #max(minimum_size, base_balancing_volume)
+        order_size_bid = max(minimum_size, quote_balancing_volume)
+        order_size_ask = max(minimum_size, base_balancing_volume)
 
         order_size_bid = max(minimum_size, self.connectors[self.exchange].quantize_order_amount(self.trading_pair, order_size_bid))
         order_size_ask = max(minimum_size, self.connectors[self.exchange].quantize_order_amount(self.trading_pair, order_size_ask))
