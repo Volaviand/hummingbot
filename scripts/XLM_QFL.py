a6de9859b78c2b9307934dae2322ad31a930cc8d
Volaviand <34086127+Volaviand@users.noreply.github.com>
2024-09-19 01:04:34 -0600

    s

--- scripts/XLM_QFL.py
+++ scripts/XLM_QFL.py
@@ -905,20 +905,17 @@ class SimplePMM(ScriptStrategyBase):
 
 
         minimum_size = self.connectors[self.exchange].quantize_order_amount(self.trading_pair, self.order_amount)
-
+        order_size_bid = quote_balancing_volume
+        order_size_ask = base_balancing_volume
         if quote_balancing_volume < minimum_size * 0.9 :
             msg_b = (f"Order Size Bid is too small for trade {order_size_bid:8f}")
             self.log_with_clock(logging.INFO, msg_b) 
-
-            order_size_bid = quote_balancing_volume
         else:
             order_size_bid = np.maximum(quote_balancing_volume , minimum_size )
 
         if base_balancing_volume < minimum_size * 0.9 :
             msg_a = (f"Order Size Ask is too small for trade {order_size_ask:8f}")
             self.log_with_clock(logging.INFO, msg_a)  
-
-            order_size_ask = base_balancing_volume 
         else:
             order_size_ask = np.maximum(base_balancing_volume , minimum_size )
 
