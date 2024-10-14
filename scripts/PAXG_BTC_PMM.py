82c73cea9c99b8acc69fc1b601fd25fd648ab0d4
Volaviand <34086127+Volaviand@users.noreply.github.com>
2024-10-14 12:53:19 -0600

    increase refresh time

--- scripts/PAXG_BTC_PMM.py
+++ scripts/PAXG_BTC_PMM.py
@@ -270,8 +270,8 @@ class SimplePMM(ScriptStrategyBase):
     def __init__(self, connectors: Dict[str, ConnectorBase]):
         super().__init__(connectors)
 
-        min_refresh_time = 30
-        max_refresh_time = 120
+        min_refresh_time = 90
+        max_refresh_time = 300
 
         # Generate a random integer between min and max using randint
         self.order_refresh_time = random.randint(min_refresh_time, max_refresh_time)
@@ -279,7 +279,7 @@ class SimplePMM(ScriptStrategyBase):
         self.last_time_reported = 0
 
 
-        self.garch_refresh_time = 480 ### Same as volatility interval
+        self.garch_refresh_time = 600 
         self_last_garch_time_reported = 0
 
 
