7be6f7925bf12a3e17d3e5427cb0e022b8d77caa
Volaviand <34086127+Volaviand@users.noreply.github.com>
2024-10-13 22:37:33 -0600

    reintroduce baselines. have cpool go off of mid price

--- scripts/PAXG_BTC_PMM.py
+++ scripts/PAXG_BTC_PMM.py
@@ -477,7 +477,9 @@ class SimplePMM(ScriptStrategyBase):
 
 
         df['Mid Line'] = (df['High Line'] + df['Low Line']) / 2
-
+        
+        self._bid_baseline = df['Low Line'].iloc[-1]
+        self._ask_baseline = df['High Line'].iloc[-1]
         return df
 
     def call_trade_history(self, file_name='trades_PAXG_BTC.csv'):
