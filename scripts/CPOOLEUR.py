19106f605bf873ffd1c7897fe68f4a05f54343a3
Volaviand <34086127+Volaviand@users.noreply.github.com>
2024-11-04 20:58:38 -0700

    t

--- scripts/CPOOLEUR.py
+++ scripts/CPOOLEUR.py
@@ -1634,9 +1634,9 @@ class SimplePMM(ScriptStrategyBase):
             # Handle the case where min and max sizes are the same
             if min_order_size == max_order_size:
                 max_full_orders = 5 # calculate_max_orders(min_order_size, max_order_size)
-                max_full_distance = max_levels * max_full_orders
+                max_full_distance = 5 # max_levels * max_full_orders
                 # Control the order depth downwards to 5 orders
-                max_full_distance = int(max_full_distance)
+                max_full_distance = 5 # int(max_full_distance)
 
                 trade_direction_flag = True
                 for level in range(max_full_distance):
