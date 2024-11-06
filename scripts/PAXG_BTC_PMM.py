4dbd63b574a7864240932abd4d52793df19e9071
Volaviand <34086127+Volaviand@users.noreply.github.com>
2024-11-06 13:20:03 -0700

    h

--- scripts/PAXG_BTC_PMM.py
+++ scripts/PAXG_BTC_PMM.py
@@ -28,29 +28,29 @@ print("Import successful!")
     # rolling_periods: int = 12 ,
     # trading_style: str = 'QFL'):
 
-# # Initialize bot with one configuration
-# KRAKENQFLBOT(\
-#     trading_pair= "PAXG-BTC",
-#     exchange= "kraken",
-#     base_asset= "PAXG",
-#     quote_asset= "BTC",
-#     history_market = "PAXGXBT",
-#     min_profitability= Decimal(0.015),
-#     buy_p = Decimal(0.975),
-#     sell_p = Decimal(1.025),
-#     quote_order_amount= Decimal(0.0001),
-#     order_amount = Decimal(0.002),
-#     max_order_amount = Decimal(0.01),
-#     maximum_orders = 50,
-#     inv_target_percent = Decimal(0.50),
-#     order_shape_factor = Decimal(2.0),
-#     history_name = 'PAXGXBT_60.csv',
-#     trade_history_name = 'trades.PAXG_BTC',
-#     chart_period = '60',
-#     volatility_periods = 168,
-#     rolling_periods = 12 ,
-#     # Styles = QFL , Account Building
-#     trading_style = 'QFL')
+# Initialize bot with one configuration
+KRAKENQFLBOT(\
+    trading_pair= "PAXG-BTC",
+    exchange= "kraken",
+    base_asset= "PAXG",
+    quote_asset= "BTC",
+    history_market = "PAXGXBT",
+    min_profitability= Decimal(0.015),
+    buy_p = Decimal(0.975),
+    sell_p = Decimal(1.025),
+    quote_order_amount= Decimal(0.0001),
+    order_amount = Decimal(0.002),
+    max_order_amount = Decimal(0.01),
+    maximum_orders = 50,
+    inv_target_percent = Decimal(0.50),
+    order_shape_factor = Decimal(2.0),
+    history_name = 'PAXGXBT_60.csv',
+    trade_history_name = 'trades.PAXG_BTC',
+    chart_period = '60',
+    volatility_periods = 168,
+    rolling_periods = 12 ,
+    # Styles = QFL , Account Building
+    trading_style = 'QFL')
 
 
 # import logging
