47032d33118ee2f135b53e586571daa7000d7945
Volaviand <34086127+Volaviand@users.noreply.github.com>
2024-11-06 14:15:53 -0700

    print to test

--- hummingbot/qfl/Kraken_QFL_Bot.py
+++ hummingbot/qfl/Kraken_QFL_Bot.py
@@ -691,7 +691,7 @@ class KRAKENQFLBOT(ScriptStrategyBase):
     """
     
     def __init__(self, 
-    # connectors: Dict[str, ConnectorBase],  # Default to None
+    connectors: Dict[str, ConnectorBase],  # Default to None
     trading_pair: str = "PAXG-BTC",
     exchange: str = "kraken",
     base_asset: str = "PAXG",
@@ -714,10 +714,13 @@ class KRAKENQFLBOT(ScriptStrategyBase):
     trading_style: str = 'QFL'):
       
 
-
-
+        # If connectors are not passed, initialize it here, you can set it to None or any default value.
+        if connectors is None:
+            connectors = {}  # Empty dictionary or any default value
+        # Debug: Print the connectors value
+        print("Connectors:", connectors)
         # Call the parent class's constructor to handle the connectors
-        super().__init__()
+        super().__init__(connectors)
         # Debug: Print the connectors value
         print("Connectors:", self.connectors)
         # self.connectors = connectors
